b2a1273a3c0c92e5ada94fa63ad16d8636152317
limeniye <limeniye@gmail.com>
2022-08-02 17:08:50 +0300

    Added Zhyttya.UI.Maui

---
+++ src/Zhyttya.UI.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Zhyttya.UI.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Zhyttya.UI.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Zhyttya.UI.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
