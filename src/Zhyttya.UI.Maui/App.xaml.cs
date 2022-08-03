fdbfe51d50b11e09444f61aba68283fc2730b107
limeniye <limeniye@gmail.com>
2022-08-03 23:35:00 +0300

    - Window debug sizing;

--- src/Zhyttya.UI.Maui/App.xaml.cs
+++ src/Zhyttya.UI.Maui/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace Zhyttya.UI.Maui
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace Zhyttya.UI.Maui;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    const int WindowWidth = 450;
+    const int WindowHeight = 750;
+
+    public App()
+    {
+        InitializeComponent();
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS && DEBUG
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32(WindowWidth, WindowHeight));
+#endif
+        });
+
+        MainPage = new AppShell();
     }
 }
