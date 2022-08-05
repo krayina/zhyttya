9c37beafb8d4c4e58f722e261ae93cb2d3b0c153
limeniye <limeniye@gmail.com>
2022-08-05 19:30:42 +0300

    - Added Uno;

---
+++ src/Zhyttya.UI.Uno/Zhyttya.UI.Uno/Zhyttya.UI.Uno.Shared/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Zhyttya.UI.Uno
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
